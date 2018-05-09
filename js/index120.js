c7708908ca5b3ac5e09e0fc8ab70929625334649
Anna Golebiewska <andzia.golebiewska@gmail.com>
2018-05-09 12:37:35 -0500

    add exe 120-122

---
+++ js/index120.js
@@ -0,0 +1,12 @@
+const students = [];
+
+students[0] = 'Marta';
+students[1] = 'Nico';
+students[2] = 'tri';
+students[3] = 'Anton';
+students[4] = 'Nana';
+students[5] = 'Hugo';
+
+for (let index = 0; index < 5; index++) {
+   console.log(`index ${index}, ${students[index]}`);
+}
---
+++ js/index122.js
@@ -0,0 +1,15 @@
+
+let ingredients = ['Pork', 'Pomato', 'Chicken', 'Lettuce', 'Beef', 'Carrots', 'Cucumber'];
+
+
+
+let vegetarian = [];
+
+vegetarian [0] = ingredients [1];
+vegetarian [1] = ingredients [3];
+vegetarian [2] = ingredients [5];
+vegetarian [3] = ingredients [6];
+
+
+console.log('Ingrediants list: ' + ingredients[0], ingredients[2], ingredients[4]);
+console.log(`Vegetarian list: ` + vegetarian[0], vegetarian [1], vegetarian [2], vegetarian [3] );
